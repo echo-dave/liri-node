87d03ea4018b35271a8f89767683c9c1f1d86aa5
Echo <echo.dave@protonmail.com>
2019-11-11 22:08:09 -0500

    corrected date time api pull

--- liri.js
+++ liri.js
@@ -84,7 +84,7 @@ Performer:
 venue:
   ${response.data.events[i].venue.name}
   ${response.data.events[i].venue.city}
-  ${response.data.events[i].venue.datetime_local}
+  ${response.data.events[i].datetime_local}
 ___________  `);
         }
 
