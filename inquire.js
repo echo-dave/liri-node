295811ee1b7db866ce2cf3ef832a62f11d5c4eae
Dave May <echo.dave@protonmail.com>
2019-11-13 15:05:19 -0500

    inquisiter fully functional with when statement for by passing search query on do what it says

--- inquire.js
+++ inquire.js
@@ -3,19 +3,22 @@ inquirer = require('inquirer');
 
 inquirer.prompt([
     {
-        type:"list",
-        message:"what would you like to search for?",
-        choices:["spotify-this-song", "concert-this", "movie-this","do-what-it-says"],
-        default:"randomDo",
-        name:"commandInput"
+        type: "list",
+        message: "what would you like to search for?",
+        choices: ["spotify-this-song", "concert-this", "movie-this", "do-what-it-says"],
+        default: "do-what-it-says",
+        name: "commandInput"
     },
     {
-    type:"input",
-    message:"What do you want to search for?",
-    name:"searchTerm"
+        type: "input",
+        message: "What do you want to search for?",
+        name: "searchTerm",
+        when: function (answers) {
+            return answers.commandInput !== "do-what-it-says"
+        }
     }
-    
-]).then(function (answers){
-    search.switchCases(answers.commandInput,answers.searchTerm);
+
+]).then(function (answers) {
+    search.switchCases(answers.commandInput, answers.searchTerm);
     // console.log(answers);
 })
